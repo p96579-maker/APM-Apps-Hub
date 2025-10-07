2a4f732799273872feb637efc67b2434ef4fca67
Ball <p96579@gmail.com>
2025-10-07 19:35:11 +0800

    Add files via upload

---
+++ sw-hub.js
@@ -0,0 +1,37 @@
+
+const CACHE = 'apm-hub-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith((async () => {
+    const cache = await caches.open(CACHE);
+    const cached = await cache.match(req);
+    if (cached) return cached;
+    try {
+      const res = await fetch(req);
+      cache.put(req, res.clone());
+      return res;
+    } catch (err) {
+      if (req.mode === 'navigate') {
+        const index = await cache.match('./index.html');
+        if (index) return index;
+      }
+      throw err;
+    }
+  })());
+});
